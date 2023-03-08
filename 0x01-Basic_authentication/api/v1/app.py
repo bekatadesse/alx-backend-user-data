e3bb0bc0ab7c7eba3f43169a3ce11ef0cc0ce315
bekatadesse <bekatadesse5@gmail.com>
2023-03-08 04:06:51 +0300

    task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -33,6 +33,23 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.before_request
+def before_request() -> str:
+    """Method before_request
+    """
+    if auth is not None:
+        if not auth.require_auth(request.path, ['/api/v1/status/',
+                                                '/api/v1/unauthorized/',
+                                                '/api/v1/forbidden/']):
+            return
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+    else:
+        return
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
