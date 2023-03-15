8243f373590445768dd6e5ea8822233c0ab5e8b1
bekatadesse <bekatadesse5@gmail.com>
2023-03-15 13:18:49 +0000

    fix pycodestyle style

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds user to database
         Return: User Object
